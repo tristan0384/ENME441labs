925a65a38f02b7b6a1e1d99a7f13e90ce113c360
tristan0384 <tristan0384@gmail.com>
2025-10-07 22:47:15 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -2,6 +2,22 @@ import RPi.GPIO as GPIO
 import time
 from math import *
 GPIO.setmode(GPIO.BCM)
+x=1
+
+GPIO.add_event_detect(
+	26,
+	gpio.RISING,
+	callback=change(-1),
+	bouncetime=(1000))
+GPIO.add_event_detect(
+	26,
+	gpio.RISING,
+	callback=change(1),
+	bouncetime=(1000))
+
+def change(i):
+	global x=i
+
 pins=[]
 
 print(f"Started")
@@ -11,15 +27,18 @@ for i in range(2,12):
 	pins.append(GPIO.PWM(i,500))
 	pins[i-2].start(0)
 
+GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
 try:
 	while(1):
 		for i in range(len(pins)):
 			#pins[i].ChangeDutyCycle(100)
-			pins[i].ChangeDutyCycle((sin(2*pi*.2*time.time()-i*pi/11))**2)
+			pins[i].ChangeDutyCycle((sin(2*pi*.2*time.time()-i*x*pi/11))**2)
 
 except:
 	print(f"Ending")
 	GPIO.cleanup()
+
 
 
 
