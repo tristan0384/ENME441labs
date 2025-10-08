bf1442f27eb358ab597f757cdf7e379b19dde831
tristan0384 <tristan0384@gmail.com>
2025-10-07 23:05:18 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -22,12 +22,12 @@ def change(i):
 GPIO.add_event_detect(
 	26,
 	GPIO.RISING,
-	x=-1,
+	change(-1),
 	bouncetime=1000)
 GPIO.add_event_detect(
 	26,
 	GPIO.FALLING,
-	x=1,
+	change(1),
 	bouncetime=1000)
 
 try:
@@ -39,6 +39,7 @@ try:
 except:
 	print(f"Ending")
 	GPIO.cleanup()
+
 
 
 
