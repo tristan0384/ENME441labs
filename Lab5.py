c27468413a2b25f9d1c8d868636a1fa7825d799e
tristan0384 <tristan0384@gmail.com>
2025-10-07 20:25:02 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-start=time.time()
 pins=[]
+
+print(f"Started")
 
 for i in range(2,11):
 	pins.append(obj=GPIO.PWM(i,500))
@@ -11,3 +12,7 @@ for i in range(2,11):
 while(1):
 	for i in range(10):
 		pins(i).ChangeDutyCycle((sin(2*pi*.2*time.time()-i*pi/11))^2)
+
+except:
+	print(f"Ending")
+	GPIO.cleanup()
