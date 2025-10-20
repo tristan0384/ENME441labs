7abae1adeba8dbc1cbcd50fddab0f2b3e41fb823
tristan0384 <tristan0384@gmail.com>
2025-10-20 02:33:40 -0400

    Update Lab6.py

--- Lab6.py
+++ Lab6.py
@@ -1,13 +1,21 @@
 import time
 import shifter
+import random
 import RPi.GPIO as GPIO
 
 shift = shifter.Shifter(23,24,25)
-
+x=0
 
 try:
   while 1:
-  	shift.shiftByte()
+  	val=random.randrange(-1,2,2)
+  	x=x+val
+  	if x<0 or x>7:
+  		shift.shiftByte(-val)
+  		shift.shiftByte(-val)
+  		x=x-2*val
+  	else:
+  		shift.shiftByte(val)
   	time.sleep(.05)
 
 except KeyboardInterupt:
