036aca51bef1f1c5c8791b3950380cdbf26ca097
tristan0384 <tristan0384@gmail.com>
2025-11-12 01:56:48 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -37,7 +37,7 @@ class Stepper:
     shifter_outputs = 0   # track shift register outputs for all motors
     seq = [0b0001,0b0011,0b0010,0b0110,0b0100,0b1100,0b1000,0b1001] # CCW sequence
     delay = 1200          # delay between motor steps [us]
-    steps_per_degree = 4096/360    # 4096 steps/rev * 1/360 rev/deg
+    steps_per_degree = (4096*2)/360    # 4096 steps/rev * 1/360 rev/deg
 
     def __init__(self, shifter, lock):
         self.s = shifter           # shift register
